536d49cc1242f6b23fcb47e7868d9a572248157b
xssayy <nikiatanik@gmail.com>
2024-01-15 05:11:25 +0200

    finished 2of6 tasks

---
+++ js/task-1.js
@@ -0,0 +1,26 @@
+const categoriesList = document.querySelector(`#categories`);
+categoriesList.classList.add(`categories-list`);
+
+const categoriesArray = categoriesList.querySelectorAll(`li.item`);
+
+categoriesArray.forEach((category) => {
+  const categoryListArray = category.querySelectorAll(`ul`);
+  for (const categoryList of categoryListArray) {
+    categoryList.classList.add(`category-list`);
+  }
+});
+
+categoriesArray.forEach((category) => {
+  const categoryListItemArray = category.querySelectorAll(`li`);
+  for (const categoryListItem of categoryListItemArray) {
+    categoryListItem.classList.add(`category-list-item`);
+  }
+});
+
+console.log(`Number of categories: ${categoriesArray.length}`);
+categoriesArray.forEach((category) => {
+  const categoryTitle = category.querySelector(`h2`);
+  const categoryItemsArray = category.querySelectorAll("ul > li");
+  console.log(`Category: ${categoryTitle.textContent}`);
+  console.log(`Elements: ${categoryItemsArray.length}`);
+});
